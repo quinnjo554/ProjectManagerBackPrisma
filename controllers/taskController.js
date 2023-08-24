bf11b21563034e2b055f034c032b62676a5311b1
Quinn Johnson <quinnjohnson24@gmail.com>
2023-08-23 22:35:28 -0500

    added task endpoint for searching by status

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,50 +1,66 @@
-class TaskService{
-    constructor(prisma){
-        this.prisma = prisma
+class TaskService {
+  constructor(prisma) {
+    this.prisma = prisma;
+  }
+
+  async getTaskByProjId(projectId) {
+    const project = await this.prisma.project.findUnique({
+      where: { project_id: parseInt(projectId) },
+    });
+    if (!project) {
+      throw new Error("Custom needed");
     }
 
-    async getTaskByProjId(projectId){
-            const project =await this.prisma.project.findUnique({
-                where: { project_id: parseInt(projectId) },
-            });
-            if (!project) {
-              throw new Error('Custom needed');
-            }
-        
-            const tasks = await this.prisma.task.findMany({
-              where: { project_id: project.project_id },
-            });
-            const users = [];
-            for (const task of tasks) {
-              // Retrieve the user for each task's assignee_id
-              const user = await this.prisma.user.findUnique({
-                where: { user_id: task.assignee_id },
-              });
-              // Add the user to the users array
-              users.push(user);
-            }
-          
-            return {tasks,users};
+    const tasks = await this.prisma.task.findMany({
+      where: { project_id: project.project_id },
+    });
+    const users = [];
+    for (const task of tasks) {
+      // Retrieve the user for each task's assignee_id
+      const user = await this.prisma.user.findUnique({
+        where: { user_id: task.assignee_id },
+      });
+      // Add the user to the users array
+      users.push(user);
     }
 
-async getTaskForUser(userId){
+    return { tasks, users };
+  }
+
+  async getTaskForUser(userId) {
     const user = await this.prisma.user.findUnique({
       where: { user_id: userId },
     });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     const tasks = await this.prisma.task.findMany({
       where: { assignee_id: userId },
       take: 10, // Limit to 10 tasks, you can customize this based on your pageable settings
-      orderBy: { task_id: 'asc' }, // Sort by task_id in ascending order
+      orderBy: { task_id: "asc" }, // Sort by task_id in ascending order
     });
 
-    return ({ tasks });
-}
+    return { tasks };
+  }
+  async getTaskForProjByStatus(project_id, status) {
+    const project = await this.prisma.project.findUnique({
+      where: { project_id: parseInt(project_id) },
+    });
 
+    if (!project) {
+      throw new Error("Project not found");
+    }
 
+    const tasks = await this.prisma.task.findMany({
+      where: {
+        project_id: project.project_id,
+        status: status,
+      },
+    });
+
+    return { tasks };
+  }
 }
-module.exports = TaskService
+module.exports = TaskService;
--- routes/task.js
+++ routes/task.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const TaskService = require("../controllers/taskController"); // Correct import path
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 const taskService = new TaskService(prisma);
 
-
-router.get('/project/:id', async (req, res) => {
-    const id = req.params.id;
-      const task = await taskService.getTaskByProjId(id);
-      res.json(task);
-  });
-router.get('/user/:id', async (req,res)=>{
-    const id = parseInt(req.params.id);
-    const task = await taskService.getTaskForUser(id)
-    res.json(task);
-})
-  module.exports = router
+router.get("/project/:id", async (req, res) => {
+  const id = req.params.id;
+  const task = await taskService.getTaskByProjId(id);
+  res.json(task);
+});
+router.get("/user/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const task = await taskService.getTaskForUser(id);
+  res.json(task);
+});
+router.get("/status/:projId/:status", async (req, res) => {
+  const projId = parseInt(req.params.projId);
+  const status = req.params.status;
+  const task = await taskService.getTaskForProjByStatus(projId, status);
+  res.json(task);
+});
+module.exports = router;
